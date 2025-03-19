afe339e60e7f4a1417165a4a9ecd740c1d9a1e34
labuelsamen <luai.abuelsamen@mail.mcgill.ca>
2025-03-19 02:39:20 -0700

    save rgbd

--- sample_script.py
+++ sample_script.py
@@ -16,7 +16,7 @@ def save_image_thread(running_event):
     while running_event.is_set() or not image_queue.empty():
         if image_queue.empty():
             continue
-        run_number, img, ego, pos, tick = image_queue.get(timeout=0.1)
+        run_number, img, ego, depth_img, pos, tick = image_queue.get(timeout=0.1)
         run_folder = f"data/run_{run_number}"
         if run_number not in log:
             log[run_number] = {}
@@ -32,6 +32,9 @@ def save_image_thread(running_event):
         img_path = os.path.join(run_folder, f"ego_{tick}.png")
         img.save(img_path)
         log[run_number][tick]['ego'] = img_path
+        img_path = os.path.join(run_folder, f"depth_{tick}.png")
+        plt.imsave(img_path, depth_img)
+        log[run_number][tick]['depth'] = img_path
 
 running_event = threading.Event()
 running_event.set()
@@ -67,23 +70,23 @@ try:
         q_init_upright = np.array([0, -np.pi/2, 0, 0, np.pi/2, 0])
         env.reset()
         env.forward(q=q_init_upright, joint_idxs=env.idxs_forward)
-        R_trgt = rpy2r(np.radians([-180, 0, 90]))
+        R_trgt = rpy2r(np.radians([0,80,0]))@rpy2r(np.radians([-180,0,90]))
 
         pick_position = env.get_p_body(obj_names[0])
-        pick_position[2] += 0.005
+        pick_position[2] += 0.01
         pre_grasp_position = pick_position + np.array([0.0, 0.0, 0.1])
-        q_ik_pregrasp = env.solve_ik(body_name='tcp_link', p_trgt=pre_grasp_position, R_trgt=R_trgt, IK_P=True, IK_R=True, q_init=np.array(q_init_upright), idxs_forward=env.idxs_forward, idxs_jacobian=env.idxs_jacobian, RESET=False, DO_RENDER=False, render_every=1, th=1 * np.pi / 180.0, err_th=1e-3, w_weight=0.5)
-        q_ik = env.solve_ik(body_name='tcp_link', p_trgt=pick_position, R_trgt=R_trgt, IK_P=True, IK_R=True, q_init=np.array(q_ik_pregrasp), idxs_forward=env.idxs_forward, idxs_jacobian=env.idxs_jacobian, RESET=False, DO_RENDER=False, render_every=1, th=1 * np.pi / 180.0, err_th=1e-3, w_weight=0.5)
+        q_ik_pregrasp = env.solve_ik(body_name='tcp_link', p_trgt=pre_grasp_position, R_trgt=R_trgt, IK_P=True, IK_R=False, q_init=np.array(q_init_upright), idxs_forward=env.idxs_forward, idxs_jacobian=env.idxs_jacobian, RESET=False, DO_RENDER=False, render_every=1, th=1 * np.pi / 180.0, err_th=1e-2, w_weight=0.5)
+        q_ik = env.solve_ik(body_name='tcp_link', p_trgt=pick_position, R_trgt=R_trgt, IK_P=True, IK_R=False, q_init=np.array(q_ik_pregrasp), idxs_forward=env.idxs_forward, idxs_jacobian=env.idxs_jacobian, RESET=False, DO_RENDER=False, render_every=1, th=1 * np.pi / 180.0, err_th=1e-2, w_weight=0.5)
         post_pick_position = pick_position + np.array([0.0, 0.0, 0.1])
-        q_ik_postpick = env.solve_ik(body_name='tcp_link', p_trgt=post_pick_position, R_trgt=R_trgt, IK_P=True, IK_R=True, q_init=np.array(q_ik), idxs_forward=env.idxs_forward, idxs_jacobian=env.idxs_jacobian, RESET=False, DO_RENDER=False, render_every=1, th=1 * np.pi / 180.0, err_th=1e-3, w_weight=0.5)
+        q_ik_postpick = env.solve_ik(body_name='tcp_link', p_trgt=post_pick_position, R_trgt=R_trgt, IK_P=True, IK_R=False, q_init=np.array(q_ik), idxs_forward=env.idxs_forward, idxs_jacobian=env.idxs_jacobian, RESET=False, DO_RENDER=False, render_every=1, th=1 * np.pi / 180.0, err_th=1e-2, w_weight=0.5)
 
         q_traj_combined = np.vstack([q_init_upright, q_ik_pregrasp, q_ik])
         times, q_traj = get_interp_const_vel_traj(q_traj_combined, vel=np.radians(90), HZ=env.HZ)
 
         place_position = platform_xyz + np.array([0.0, 0.0, 0.1])
-        q_ik_place = env.solve_ik(body_name='tcp_link', p_trgt=place_position, R_trgt=R_trgt, IK_P=True, IK_R=True, q_init=np.array(q_traj[-1, :]), idxs_forward=env.idxs_forward, idxs_jacobian=env.idxs_jacobian, RESET=False, DO_RENDER=False, render_every=1, th=1 * np.pi / 180.0, err_th=1e-3, w_weight=0.5)
+        q_ik_place = env.solve_ik(body_name='tcp_link', p_trgt=place_position, R_trgt=R_trgt, IK_P=True, IK_R=False, q_init=np.array(q_traj[-1, :]), idxs_forward=env.idxs_forward, idxs_jacobian=env.idxs_jacobian, RESET=False, DO_RENDER=False, render_every=1, th=1 * np.pi / 180.0, err_th=1e-2, w_weight=0.5)
         pre_place_position = place_position + np.array([0.0, 0.0, 0.1])
-        q_ik_preplace = env.solve_ik(body_name='tcp_link', p_trgt=pre_place_position, R_trgt=R_trgt, IK_P=True, IK_R=True, q_init=np.array(q_ik_place), idxs_forward=env.idxs_forward, idxs_jacobian=env.idxs_jacobian, RESET=False, DO_RENDER=False, render_every=1, th=1 * np.pi / 180.0, err_th=1e-3, w_weight=0.5)
+        q_ik_preplace = env.solve_ik(body_name='tcp_link', p_trgt=pre_place_position, R_trgt=R_trgt, IK_P=True, IK_R=False, q_init=np.array(q_ik_place), idxs_forward=env.idxs_forward, idxs_jacobian=env.idxs_jacobian, RESET=False, DO_RENDER=False, render_every=1, th=1 * np.pi / 180.0, err_th=1e-2, w_weight=0.5)
 
         q_traj_place_combined = np.vstack([q_ik_postpick, q_ik_preplace, q_ik_place])
         times_place, q_traj_place = get_interp_const_vel_traj(q_traj_place_combined, vel=np.radians(90), HZ=env.HZ)
@@ -92,10 +95,13 @@ try:
         env.update_viewer(azimuth=66.08, distance=3.0, elevation=-50, lookat=[0.4, 0.18, 0.71], VIS_TRANSPARENT=False, VIS_CONTACTPOINT=False, contactwidth=0.05, contactheight=0.05, contactrgba=np.array([1, 0, 0, 1]), VIS_JOINT=True, jointlength=0.25, jointwidth=0.05, jointrgba=[0.2, 0.6, 0.8, 0.6])
 
         tick = 0
+        gripper_closed_steps_0 = np.tile(np.append(q_traj[0], 1), (50, 1))
         gripper_closed_steps_1 = np.tile(np.append(q_traj[-1], 0.5), (150, 1))
         gripper_closed_steps_2 = np.tile(np.append(q_traj_place[-1], 1), (150, 1))
+        
 
         q_traj_combined = np.vstack([
+            gripper_closed_steps_0,
             np.hstack([q_traj, np.ones((q_traj.shape[0], 1))]),
             gripper_closed_steps_1,
             np.hstack([q_traj_place, np.ones((q_traj_place.shape[0], 1))*0.5]),
@@ -110,12 +116,12 @@ try:
             # Get PCD from a specific view
             p_ego  = p_cam
             p_trgt = p_cam + R_cam[:,2]
-            if tick % 10 == 0:
+            if tick % 1 == 0:
                 img = env.grab_image()
                 rgb_img,depth_img,pcd,xyz_img = env.get_egocentric_rgb_depth_pcd(
                     p_ego=p_ego,p_trgt=p_trgt,rsz_rate=None,fovy=45,BACKUP_AND_RESTORE_VIEW=True)
                 pos = env.get_q([0, 1, 2, 3, 4, 5, 6])
-                image_queue.put((run_number, img, rgb_img, pos, tick))
+                image_queue.put((run_number, img, rgb_img, depth_img, pos, tick))
             q = q_traj_combined[tick, :]
             env.step(ctrl=q, ctrl_idxs=[0, 1, 2, 3, 4, 5, 6])
             env.render()
@@ -123,6 +129,7 @@ try:
 
         run_number += 1
         env.close_viewer()
+        break
 
 except KeyboardInterrupt:
     print("\nMain thread interrupted. Stopping worker thread...")
